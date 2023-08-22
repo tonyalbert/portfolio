4508f2e8feb33fae9cc8df5d7b1a859087dd7898
Tony <tony.albert789@gmail.com>
2023-08-22 20:08:11 -0300

    Card SObre mim

--- src/components/Sobre.vue
+++ src/components/Sobre.vue
@@ -5,6 +5,8 @@
                 <h1 class="text-[#27AE60] font-bold">SOBRE MIM</h1>
                 <div class="w-4 h-4 bg-[#27AE60] rounded-full"></div>
             </button>
+
+            <p class="p-4 text-white">Hello, I am a technology enthusiast who is constantly seeking new opportunities to learn and grow professionally. Currently, I possess skills in Next, React, Node.js, among other cutting-edge stacks in the market. Currently, I am a Freelance Developer who performs live coding with the intention of helping my community with tips on React, Next, Node, and many other technologies. We are also creating an Open Source project among our community with a focus on gamification-style learning for beginner developers, using agile methodologies and git flow.</p>
             <div>
 
             </div>
